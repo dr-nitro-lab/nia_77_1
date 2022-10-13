b67f9e71bb11e2c7f8809a667911e7490dba8aba
Kim Seong-Woo <54995090+tjddn0402@users.noreply.github.com>
2022-10-13 19:25:53 +0900

    code for checking valid dataset

---
+++ update_table/check_dataset.py
@@ -0,0 +1,71 @@
+import os
+import glob
+from pathlib import Path
+from argparse import ArgumentParser
+import pandas as pd
+from json_utils import (
+    compare_keys_recursively
+)
+
+
+def parse_arg():
+    parser = ArgumentParser("1. 같은 이름의 세가지 파일 [.wav, .json., .mid]이 한 세트를 이루고 있는지 확인.\n"
+                            "2. metadata 형식에 어긋나는 json파일 검출")
+    parser.add_argument("--dataset", '-d', type=str,
+                        default='220924')
+    parser.add_argument("--excel", '-e', type=str,
+                        default='220924.error.xlsx')
+    args = parser.parse_args()
+    return args
+
+
+def check_missing_files(dataset_path):
+
+    wav_files = list(dataset_path.glob("*.wav"))
+    json_files = list(dataset_path.glob("*.json"))
+    mid_files = list(dataset_path.glob("*.mid"))
+
+    # extension과 directory를 제외한 파일의 basename의 리스트로 변환
+    for i, wav in enumerate(wav_files):
+        wav_files[i] = wav.stem
+    for i, json in enumerate(json_files):
+        json_files[i] = json.stem
+    for i, mid in enumerate(mid_files):
+        mid_files[i] = mid.stem
+
+    # union
+    wav_set, json_set, mid_set = set(
+        wav_files), set(json_files), set(mid_files)
+    union = wav_set.union(json_set).union(mid_set)
+
+    # difference
+    wav_missing = union - wav_set
+    json_missing = union - json_set
+    mid_missing = union - mid_set
+
+    print(f"length of union: {len(union)}")
+    print(f"length of wav_missing: {len(wav_missing)}")
+    print(f"length of json_missing: {len(json_missing)}")
+    print(f"length of mid_missing: {len(mid_missing)}")
+
+    return pd.DataFrame({
+        "wav": pd.Series(sorted(list(wav_missing))),
+        "json": pd.Series(sorted(list(json_missing))),
+        "mid": pd.Series(sorted(list(mid_missing)))
+    })
+
+
+def check_error_jsons(dataset_path):
+    return pd.DataFrame()
+
+
+if __name__ == "__main__":
+    args = parse_arg()
+
+    missing_table = check_missing_files(Path(args.dataset))
+    error_table = check_error_jsons(Path(args.dataset))
+
+    writer = pd.ExcelWriter(args.excel)
+    missing_table.to_excel(writer, sheet_name="누락파일목록")
+    error_table.to_excel(writer, sheet_name="에러json파일목록")
+    writer.save()
