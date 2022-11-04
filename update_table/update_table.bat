7206e40a3cdc9b7a3c27fff950723a51df9e0b29
Dr.Nitro <nitrobitz@naver.com>
2022-11-05 00:38:39 +0900

    update_table: splited to [nia_77_1_summary] repository

--- update_table/update_table.bat
+++
@@ -1,6 +0,0 @@
-@REM set your dataset path & excel file path.
-@REM if your excel file path is None, default path is same with dataset path.
-chcp 65001
-python update_count_table.py
-python update_time_table.py 
-python update_error_table.py
